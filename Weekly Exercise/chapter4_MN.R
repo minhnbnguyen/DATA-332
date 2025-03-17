8f1a775f8b40493808bddd378e31b2b3fe511c8d
Robin (Minh) Nguyen <142035875+minhnbnguyen@users.noreply.github.com>
2025-03-16 21:37:43 -0500

    Add files via upload

---
+++ Weekly Exercise/chapter4_MN.R
@@ -0,0 +1,35 @@
+#Minh Nguyen Chapter 4
+cards <- data.frame(face = c("ace","two","six"),
+                    suit = c("clubs","clubs","clubs"), value = c(1, 2, 3),
+                    stringsAsFactors = FALSE)
+
+deck <- data.frame(
+  face = c("king", "queen", "jack", "ten", "nine", "eight", "seven", "six",
+           "five", "four", "three", "two", "ace", "king", "queen", "jack", "ten",
+           "nine", "eight", "seven", "six", "five", "four", "three", "two", "ace",
+           "king", "queen", "jack", "ten", "nine", "eight", "seven", "six", "five",
+           "four", "three", "two", "ace", "king", "queen", "jack", "ten", "nine",
+           "eight", "seven", "six", "five", "four", "three", "two", "ace"),
+  suit = c("spades", "spades", "spades", "spades", "spades", "spades",
+           "spades", "spades", "spades", "spades", "spades", "spades", "spades",
+           "clubs", "clubs", "clubs", "clubs", "clubs", "clubs", "clubs", "clubs",
+           "clubs", "clubs", "clubs", "clubs", "clubs", "diamonds", "diamonds",
+           "diamonds", "diamonds", "diamonds", "diamonds", "diamonds", "diamonds",
+           "diamonds", "diamonds", "diamonds", "diamonds", "diamonds", "hearts",
+           "hearts", "hearts", "hearts", "hearts", "hearts", "hearts", "hearts",
+           "hearts", "hearts", "hearts", "hearts", "hearts"),
+  value=c(13,12,11,10,9,8,7,6,5,4,3,2,1,13,12,11,10,9,8, 7,6,5,4,3,2,1,13,12,11,10,9,8,7,6,5,4,3,2,1,13,12,11, 10,9,8,7,6,5,4,3,2,1)
+)
+#exercise 1
+deal <- function(cards) {
+  cards[1, ]
+}
+deal(deck)
+#exercise 2
+shuffle <- function(cards) {
+  random <- sample(1:52, size = 52)
+  cards[random, ]
+}
+
+deck <- shuffle(deck)
+deal(deck)
